c16594c93dd1715c6671d44f281fd33ee1a1094e
bensaei <bensaei@github.com>
2018-01-25 15:49:34 -0800

    Parser Done

--- parser.R
+++ parser.R
@@ -1,3 +1,6 @@
+start <- 1
+end <- 10
+condition <- TRUE
 Head<- scan("C:\\Users\\TekBots\ 1\\Desktop\\R-Parser\\2002\\checklists.csv",what="numeric",nlines=1,sep=',',skip=0,quiet=TRUE)
 outHead<-NULL
 for (i in 1:19)
@@ -7,14 +10,18 @@ for (i in 1:19)
 out <- NULL
 out <- append(out, list(outHead))
 #while (Data<- scan("C:\\Users\\TekBots\ 1\\Desktop\\2002\\checklists.csv",what="numeric",nlines=1,sep=',',skip=0,quiet=TRUE))
-for (i in 1:10)
+for (i in start:end)
 {
 	Data <- scan("C:\\Users\\TekBots\ 1\\Desktop\\R-Parser\\2002\\checklists.csv",what="numeric",nlines=1,sep=',',skip=i,quiet=TRUE)
-	line <- NULL
-	for(i in 1:19)
-		line <-append(line, Data[i])
-	out <- append(out, list(line))
+	if(condition)
+	{
+		line <- NULL
+		for(i in 1:19)
+			line <-append(line, Data[i])
+		out <- append(out, list(line))
+	}
 }
-out
-
-write.csv(out, file = "C:\\Users\\TekBots\ 1\\Desktop\\R-Parser\\Output.csv")
+numCols <- length(outHead)
+outMatrix <- matrix(unlist(out), ncol = numCols , byrow = TRUE)
+outMatrix 
+write.csv(outMatrix , file = "C:\\Users\\TekBots\ 1\\Desktop\\R-Parser\\Output.csv")
